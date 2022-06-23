73282142fcec67d96d0471069b2340dd07cdb307
Jorge Bustamante Herrera <jbusth@gmail.com>
2022-06-22 19:54:03 -0500

    Se agrega un temporizador para que el programa termine cuando se cumplan 5 horas, se cambia el formato de salida del archivo .dat para que lo pueda leer de manera inmediata desde el progrma latex para hacer su gráfico.

--- termometer.py
+++ termometer.py
@@ -6,43 +6,50 @@ import datetime
 from datetime import date
 
 #inicia el método añadir archivo
-def añadir_dato(a,b,c):
+def añadir_dato(i,a,b,c):
     
     f = open("registro.dat",'a')
     f.write('\n')
-    f.write('{} {} {} {}'.format(datetime.datetime.now().time(),a,b,c))
+    f.write('{} {} {} {}'.format(i,a,b,c))
     f.close()
     
 def añadir_fecha():
     f =open("registro.dat",'a')
-    f.write('\n')
-    f.write('\n')
-    f.write('{}'.format(date.today()))	# Asigno la fecha a hoy
-    f.write('\n')
+    f.write('%\n')
+    f.write('%\n')
+    f.write('%{} {}'.format(date.today(),datetime.datetime.now().time()))	# Asigno la fecha y hora
+    f.write('%\n')
+    f.close()
+
+def añadir_encabezado():
+    f = open("registro.dat",'a')
+    f.write('minuto ambiente colector torre\n')
     f.close()
 
 # inicia funci{on principal
 añadir_fecha()
+añadir_encabezado()
 # Ajusta el número del canal y el chip SPI selecciona el dispositivo. / Set up channel number and SPI chip select device
 ambiente = MCP3008(channel=0)
 salida_colector = MCP3008(channel = 1)
 salida_torre = MCP3008(channel = 2)
-while True:
+i=0
+while (i<=300):
     # Covierte el voltaje analógico a digital y hace la conversión a celcius / Converts ACD voltage to temperature in Celsius
     a = round((ambiente.value * 3.3) * 100, 2)
     b = round((salida_colector.value * 3.3) * 100, 2)
     c = round((salida_torre.value * 3.3) * 100, 2)
 
     # se guarda en el archivo .dat
-    añadir_dato(a,b,c)
+    añadir_dato(i,a,b,c)
+    i+=1
 
 
     # Muestra en pantalla los valores obtenidos / Print both temperatures
-    print('{} {} {}'.format(a, b, c))
+    print('{} {} {} {}'.format(i,a, b, c))
 
     # Espera 60 segundos para la siguiente lectura. / Wait 60 seconds for the next read
     sleep(60)
 
-
-
-
+añadir_fecha()
+sys.exit("Se han registrado 300 minutos. Fin del programa.")
